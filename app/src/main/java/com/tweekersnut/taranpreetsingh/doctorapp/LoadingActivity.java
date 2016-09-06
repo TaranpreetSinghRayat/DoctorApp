25d03b91588db5b2c228f68d4c5493a9ed2c7742
fear fear <fear fear>
2016-09-06 13:02:48 +0530

    Loading Screen :- icon Added Full screen activity added

--- app/src/main/java/com/tweekersnut/taranpreetsingh/doctorapp/LoadingActivity.java
+++ app/src/main/java/com/tweekersnut/taranpreetsingh/doctorapp/LoadingActivity.java
@@ -2,6 +2,7 @@ package com.tweekersnut.taranpreetsingh.doctorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
