964f79cfd5de2e55a29aa19a71154003b35a47ae
fear fear <fear fear>
2016-09-06 15:12:17 +0530

    Loading Screen Check Method

--- app/src/main/java/com/tweekersnut/taranpreetsingh/doctorapp/LoadingActivity.java
+++ app/src/main/java/com/tweekersnut/taranpreetsingh/doctorapp/LoadingActivity.java
@@ -1,12 +1,17 @@
 package com.tweekersnut.taranpreetsingh.doctorapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.net.InetAddress;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -30,10 +35,38 @@ public class LoadingActivity extends AppCompatActivity {
         loadingScreenHandler.post(new Runnable() {
             @Override
             public void run() {
-                loadingLbl.setText("Checking is doctors available");
-
+                loadingLbl.setText("Checking Internet Connection!");
+                boolean internetStatus = isNetworkConnected();
+                if(internetStatus){
+                    createPop("Internet Connected!");
+                }else{
+                    createError("Error Occur","Error : Internet Connection not found.","Exit","Open Settings");
+                }
             }
         });
+    }
+
+    /*
+        Check is internet connection available
+     */
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+
+    /*
+        Create Toast
+     */
+    public void createPop(String msg){
+        Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_LONG).show();
+    }
+
+    /*
+        Create error Box with message and options.
+     */
+    public void createError(String title,String msg,String yes,String no){
+
     }
 
     @Override
